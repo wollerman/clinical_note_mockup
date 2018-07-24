10ee6261db3fda7f59bf83437d08bad111107a7a
Matt Wollerman <mjw105@case.edu>
2018-07-23 19:26:51 -0700

    add referral modals

--- src/app/referral-list/referral-list.component.ts
+++ src/app/referral-list/referral-list.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {Problem} from '../problem-list/problem-list.component';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-referral-list',
@@ -7,15 +9,57 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ReferralListComponent implements OnInit {
 
-  referralList: Referral[];
+  referralTypeOptions = [
+    'PCP'
+  ];
 
-  constructor() {
+  referralList: Referral[];
+  public modalRef: BsModalRef;
+  public newReferralName: string;
+  public newReferralType: string;
+  public newReferralStatus: string;
+  public newReferralInstructions: string;
+
+  public focused: Referral;
+
+  constructor(private modalService: BsModalService) {
     this.referralList = [
       new Referral('Dr. Jones', 'Radiologist', 'pending')
     ];
   }
 
   ngOnInit() {
+  }
+
+  public openModal(template: TemplateRef<any>, referral?: Referral) {
+    this.modalRef = this.modalService.show(template);
+    this.focused = referral;
+  }
+
+  public onConfirm() {
+    this.modalRef.hide();
+    if (this.newReferralName) {
+      this.referralList.push(
+        new Referral(
+          this.newReferralName,
+          this.newReferralType,
+          this.newReferralStatus,
+          this.newReferralInstructions
+        )
+      );
+    }
+  }
+
+  public remove() {
+    if (this.focused) {
+      this.referralList.splice(this.referralList.indexOf(this.focused), 1);
+    }
+    this.modalRef.hide();
+  }
+
+  public dismiss() {
+    this.focused = undefined;
+    this.modalRef.hide();
   }
 
 }
@@ -26,12 +70,13 @@ export class Referral {
   name: string;
   type: string;
   status: string;
-  intructions: string;
+  instructions: string;
 
-  constructor(name: string, type?: string, status?: string) {
+  constructor(name: string, type?: string, status?: string, instructions?: string) {
     this.name = name;
     this.type = type ? type : '';
-    this.status = status ? status: '';
+    this.status = status ? status : '';
+    this.instructions = instructions;
   }
 
 }
